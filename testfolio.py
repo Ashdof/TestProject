436b88e587c5bc1b7d0c714329f16805ecd9af9f
Ashdof <mzigah4@gmail.com>
2022-08-20 21:54:20 +0000

    Two Edits Made 1. Updated the comment section at the top of the page with date, developer information,  programme name and description
    2. Moved the line of code that requests for amount down after the if statement that checks
    for the input of terminator symbols. This is to strictly skip the amount question if the user
    inputs any of the terminator symbols when the name is requested
    
    3. Deleted "quest2 in terminator" from the if statement

--- testfolio.py
+++ testfolio.py
@@ -1,5 +1,13 @@
 """
 	=======================		PRE-PREPARATION FOR PORTFOLIO PROJECT	=================================
+
+	DATE:						20-AUG-2022
+	DEVELOPER:					EMMANUEL ENCHILL
+	PROGRAMME NAME:				BASIC DUES COLLECTION APPLICATION
+	PROGRAMME DESCRIPTION:		THIS IS A SIMPLE FINANCE MANAGEMENT APPLICATION. ITS SOLE CAPABILITY IS
+								ACCEPTING ENTRIES OF MEMBER NAMES AND PAYMENT AMOUNTS. PROGRAMME INITIALY 
+								LACK ERROR CHECKING CAPABILITIES; IT WILL CRASH WHEN A STRING IS PROVIDED AS
+								THE AMOUNT VALUE. MORE FEATURES WILL BE ADDED AS DEVELOPMENT ADVANCES
 """
 
 #	Function to record member contributions
@@ -13,10 +21,11 @@ def recordDues():
 	
 	while True:
 		quest1 = input(memberName)
-		quest2 = input(paidAmount)
 		
-		if quest1 in terminator or quest2 in terminator:
+		if quest1 in terminator:
 			break
+		
+		quest2 = input(paidAmount)
 		
 		# records[memberName] = paidAmount
 		records.update({quest1: quest2})	#	Update record with new information
